e3d5a8fcd8c3b6a40b2ea49b9e3f8f88d175ec39
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-01 21:52:51 -0400

    Variables.

--- main.dart
+++ main.dart
@@ -1,6 +1,25 @@
 void main() {
-  print("There once was a man named Stanley.");
-  print("He was 70 years old.");
-  print("He really liked the name Stanley.");
-  print("But he didn't like being 70.");
+  String characterName = "Jim";
+  String characterAge = "35";
+
+  print("There once was a man named ${characterName}.");
+  print("He was ${characterAge} years old.");
+
+  characterName = "Dwight";
+
+  print("He really liked the name ${characterName}.");
+  print("But he didn't like being ${characterAge}.");
 }
+// use variables when character appears multiple times.
+
+/* Variables are defined as "storage locations" for data in a program.
+    They are a way of naming information for later usage.
+
+   In order to store a value, the program needs to establish a memorable
+    (named) place (data structure) that it can use to hold the data.
+    These locations are called variables and constants.
+*/
+
+/* String interpolation is the term for the dollar sign and curly brackets.
+    E.g. $(characterName) 
+*/
