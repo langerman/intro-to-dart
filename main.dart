7795494ba4702a79cda1eefc22ac0ef97710741e
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-02 22:57:44 -0400

    Data types

--- main.dart
+++ main.dart
@@ -1,25 +1,11 @@
 void main() {
-  String characterName = "Jim";
-  String characterAge = "35";
+  String firstName = "Daniel";
+  int age = 24;
+  double gpa = 3.5;
+  bool isRegisteredVoter = false;
 
-  print("There once was a man named ${characterName}.");
-  print("He was ${characterAge} years old.");
-
-  characterName = "Dwight";
-
-  print("He really liked the name ${characterName}.");
-  print("But he didn't like being ${characterAge}.");
+  print(isRegisteredVoter);
 }
-// use variables when character appears multiple times.
 
-/* Variables are defined as "storage locations" for data in a program.
-    They are a way of naming information for later usage.
-
-   In order to store a value, the program needs to establish a memorable
-    (named) place (data structure) that it can use to hold the data.
-    These locations are called variables and constants.
-*/
-
-/* String interpolation is the term for the dollar sign and curly brackets.
-    E.g. $(characterName) 
+/* Strings (plain text), number, and boolean (treu false data types).
 */
