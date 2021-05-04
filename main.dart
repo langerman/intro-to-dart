2ecd443dd41b3c35e450efd2c981681463982545
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-04 12:51:04 -0400

    Building a basic calculator

--- main.dart
+++ main.dart
@@ -1,7 +1,19 @@
+import "dart:math";
 import "dart:io";
 
 void main() {
-  print("What is your name?:");
-  String username = stdin.readLineSync();
-  print("Hello ${username}");
+  print("Enter first number:");
+  String num1 = stdin.readLineSync();
+  print("Enter second number:");
+  String num2 = stdin.readLineSync();
+
+  print(double.parse(num1) + double.parse(num2));
+  //concatenation if adding two Strings
+
+  print("Enter first number:");
+  double num3 = double.parse(stdin.readLineSync());
+  print("Enter second number:");
+  double num4 = double.parse(stdin.readLineSync());
+
+  print(num1 + num2);
 }
