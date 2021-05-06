c2d6dece9638522aa8e55619f32c0a949fb87260
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-06 09:24:06 -0400

    Functions

--- main.dart
+++ main.dart
@@ -1,15 +1,14 @@
 import "dart:math";
 import "dart:io";
 
-void main() {
-  List<int> favNums = [4, 8, 15, 16, 23, 42];
-  //                   0, 1, 2,  3,  4,  5
-
-  favNums[3] = 17;
-  favNums.add(64);
-  favNums.remove(17); //called removing by value
-
-  print(favNums[3] + favNums[0]);
-  print(favNums.indexOf(15));
-  print(favNums.contains(15));
+void drawTriangle() {
+  print(" /|");
+  print("/_|");
 }
+
+void main() {
+  print("Before func");
+  drawTriangle();
+  print("After func");
+}
+//do not repeat yourself
