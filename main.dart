c977172482816ea3624a6b067fcee0a5f0477378
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-07 11:30:08 -0400

    Building a smarter calculator

--- main.dart
+++ main.dart
@@ -1,49 +1,31 @@
 import "dart:io";
 
-void main() {
-  bool isSmart = false;
-  bool isStudent = true;
-
-  if (isSmart) {
-    print("You are smart.");
-  } else {
-    print("You should study more.");
-  }
-
-//  print(!true); //negate
-
-  if (isSmart && isStudent) {
-    print("You are a smart student.");
-  } else if (isSmart && !isStudent) {
-    print("You are smart, but not a student.");
-  } else if (!isSmart && isStudent) {
-    print("You are not a smart student.");
-  } else {
-    print("You are not smart and not a student.");
-  }
-
-  if (isSmart || isStudent) {
-    print("You are either smart or a student or both.");
-  }
-
-  String greeting = "Hello";
-
-  if (greeting.contains("e")) {
-    print("The greeting has e in it.");
-  } else {
-    print("The greeting has no e.");
-  }
-
-  //if one conditional, you can avoid brackets i.e.:
-  if (greeting.contains("e"))
-    print("The greeting has e in it.");
-  else
-    print("The greeting has no e.");
-}
-/*
 String prompt(String promptText) {
   print("The promopt is: ${promptText}");
   String answer = stdin.readLineSync();
   return answer;
 }
-*/
+
+double promptDouble() {
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync());
+  return myNum;
+}
+
+void main() {
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("Enter an operation (+,-,/,*): ");
+
+  if (op == '+') {
+    print(num1 + num2);
+  } else if (op == '-') {
+    print(num1 - num2);
+  } else if (op == '/') {
+    print(num1 / num2);
+  } else if (op == '*') {
+    print(num1 * num2);
+  } else {
+    print("Invalid Operator");
+  }
+}
