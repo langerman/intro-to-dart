c7e38611c6e8086d8810b753a6ab1854bd1b5a7c
Daniel Langerman <65620306+langerman@users.noreply.github.com>
2021-05-07 11:04:26 -0400

    If statements with && || operators and else if

--- main.dart
+++ main.dart
@@ -1,16 +1,49 @@
 import "dart:io";
 
+void main() {
+  bool isSmart = false;
+  bool isStudent = true;
+
+  if (isSmart) {
+    print("You are smart.");
+  } else {
+    print("You should study more.");
+  }
+
+//  print(!true); //negate
+
+  if (isSmart && isStudent) {
+    print("You are a smart student.");
+  } else if (isSmart && !isStudent) {
+    print("You are smart, but not a student.");
+  } else if (!isSmart && isStudent) {
+    print("You are not a smart student.");
+  } else {
+    print("You are not smart and not a student.");
+  }
+
+  if (isSmart || isStudent) {
+    print("You are either smart or a student or both.");
+  }
+
+  String greeting = "Hello";
+
+  if (greeting.contains("e")) {
+    print("The greeting has e in it.");
+  } else {
+    print("The greeting has no e.");
+  }
+
+  //if one conditional, you can avoid brackets i.e.:
+  if (greeting.contains("e"))
+    print("The greeting has e in it.");
+  else
+    print("The greeting has no e.");
+}
+/*
 String prompt(String promptText) {
   print("The promopt is: ${promptText}");
   String answer = stdin.readLineSync();
   return answer;
 }
-
-void main() {
-  String color = prompt("Enter a color: ");
-  String pluralNoun = prompt("Enter a plural noun: ");
-  String celebrity = prompt("Enter a celebrity: ");
-  print(
-      "The color is ${color}, the plural noun is ${pluralNoun}, and the celebrity is ${celebrity}!");
-}
-//return type string
+*/
