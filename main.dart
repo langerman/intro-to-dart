fcef32d7b90e3d5c31244c85fa2c98d22273dc38
Daniel Langerman <daniel.langerman.nyc@gmail.com>
2021-05-14 10:59:16 -0400

    Constructors

--- main.dart
+++ main.dart
@@ -13,19 +13,24 @@ double promptDouble() {
 }
 
 class Book {
-  //class is the blueprint
-  //complex data type properties/attributes we need
+  //constructor function 'aTitle'
+  Book(String aTitle, String aAuthor, int aNumPages) {
+    //this is a special keyword
+    //this keyword referes to the object that is getting created (i.e. title)
+    this.title = aTitle;
+    this.author = aAuthor;
+    this.numPages = aNumPages;
+  }
+
   String title;
   String author;
   int numPages;
 }
 
 void main() {
-  Book hp = Book(); //this is the object
-  //the dot is the dereference operator
-  hp.title = "Sorcerers Stone";
-  hp.author = "JK Rowling";
-  hp.numPages = 300;
+  Book hp = Book("Sorcerers Stone", "JK Rowling", 300);
+  Book lotr = Book("Lord of the Rings", "Tolkien", 500);
 
   print(hp.title);
+  print(lotr.author);
 }
