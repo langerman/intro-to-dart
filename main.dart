48f67905bdd5d6b546ee76be8f6b43422f85f113
Daniel Langerman <daniel.langerman.nyc@gmail.com>
2021-05-15 10:13:30 -0400

    Class Methods

--- main.dart
+++ main.dart
@@ -12,25 +12,26 @@ double promptDouble() {
   return myNum;
 }
 
-class Book {
-  //constructor function 'aTitle'
-  Book(String aTitle, String aAuthor, int aNumPages) {
-    //this is a special keyword
-    //this keyword referes to the object that is getting created (i.e. title)
-    this.title = aTitle;
-    this.author = aAuthor;
-    this.numPages = aNumPages;
+//a method is a function that is inside of a class
+
+class Student {
+  String name;
+  double gpa;
+
+  Student(String aName, double aGpa) {
+    this.name = aName;
+    this.gpa = aGpa;
   }
 
-  String title;
-  String author;
-  int numPages;
+  bool hasHonors() {
+    return this.gpa >= 3.5;
+  }
 }
 
 void main() {
-  Book hp = Book("Sorcerers Stone", "JK Rowling", 300);
-  Book lotr = Book("Lord of the Rings", "Tolkien", 500);
+  Student jim = Student("Jim", 2.5);
+  Student pam = Student("Pam", 3.7);
 
-  print(hp.title);
-  print(lotr.author);
+  print(jim.hasHonors());
+  print(pam.hasHonors());
 }
